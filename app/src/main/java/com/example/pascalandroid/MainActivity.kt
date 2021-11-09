6fc33249bbceb5d4a21c09bc45bded2228562b6e
Jution Candra Kirana <jutionck@gmail.com>
2021-11-10 00:10:56 +0700

    [ACTIVITY] - Hands on Counter

--- app/src/main/java/com/example/pascalandroid/MainActivity.kt
+++ app/src/main/java/com/example/pascalandroid/MainActivity.kt
@@ -1,54 +1,32 @@
 package com.example.pascalandroid
 
-import android.annotation.SuppressLint
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var txtInputName: TextInputEditText
-    private lateinit var tvHelloName: TextView
-    private lateinit var btnGreeting: MaterialButton
+    private lateinit var count: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnGreeting = findViewById(R.id.btn_greeting)
-        btnGreeting.setOnClickListener { getNameFromInput() }
 
-        txtInputName = findViewById(R.id.txt_input_name)
-        tvHelloName = findViewById(R.id.tv_hello_name)
+        val btnIncrement: MaterialButton = findViewById(R.id.btn_increment)
+        val btnDecrement: MaterialButton = findViewById(R.id.btn_decrement)
+        btnIncrement.setOnClickListener { increment() }
+        btnDecrement.setOnClickListener { decrement() }
+        count = findViewById(R.id.tv_number_count)
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun getNameFromInput() {
-        val getName = txtInputName.text
-        Log.i("test", "Print getName $getName")
-
-        val messageGreeting = listOf<String>("Have nice day", "Never give up", "Keep working hard", "Keep your study")
-        val index = (0..3).random()
-        val currentMessageGreeting = messageGreeting[index]
-
-        if (getName.toString() == "") {
-            tvHelloName.text = "Make sure to enter your name"
-        } else {
-            // update textView
-            tvHelloName.text = "Hello $getName! $currentMessageGreeting"
-        }
-        // clear input text with setText
-        txtInputName.setText("")
-
-        hideKeyboard()
+    private fun increment() {
+        val currentNumber = count.text.toString().toInt()
+        count.text = (currentNumber + 1).toString()
     }
 
-    private fun hideKeyboard() {
-        val inputMethodService = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodService.hideSoftInputFromWindow(txtInputName.windowToken, 0)
+    private fun decrement() {
+        val currentNumber = count.text.toString().toInt()
+        count.text = (currentNumber - 1).toString()
     }
 }
