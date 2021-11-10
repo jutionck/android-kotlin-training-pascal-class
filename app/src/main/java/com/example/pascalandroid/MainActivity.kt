a8c1ff05cf3b3cbdcdb8a0cb458f3c44e9219eb7
Jution Candra Kirana <jutionck@gmail.com>
2021-11-10 09:32:12 +0700

    [LAYOUT] Task Calculator layout

--- app/src/main/java/com/example/pascalandroid/MainActivity.kt
+++ app/src/main/java/com/example/pascalandroid/MainActivity.kt
@@ -1,31 +1,12 @@
 package com.example.pascalandroid
 
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var txtInputName: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val btnGreeting: MaterialButton = findViewById(R.id.btn_greeting)
-        btnGreeting.setOnClickListener { sendData() }
-
-        txtInputName = findViewById(R.id.txt_input_name)
-    }
-
-    private fun sendData() {
-        val intent =  Intent(this@MainActivity, SecondActivity::class.java)
-        val sendName = txtInputName.text
-        txtInputName.text = sendName
-        intent.putExtra(SecondActivity.GET_NAME, txtInputName.text.toString())
-        startActivity(intent)
     }
 }
