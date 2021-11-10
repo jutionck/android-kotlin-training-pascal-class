786c7c5f4535956099d504ab91fb04c3241149de
Jution Candra Kirana <jutionck@gmail.com>
2021-11-10 11:19:12 +0700

    [ACTIVITY] Implement onclick event on xml

--- app/src/main/java/com/example/pascalandroid/MainActivity.kt
+++ app/src/main/java/com/example/pascalandroid/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.pascalandroid
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    @SuppressLint("SetTextI18n")
+    fun btnGreetingPress(view: android.view.View) {
+        Log.i("test", view.toString())
+        val txtInputName: TextInputEditText = findViewById(R.id.txt_input_name)
+        val tvHelloName: TextView = findViewById(R.id.tv_hello_name)
+
+        val getNameFromInput = txtInputName.text
+        tvHelloName.text = "Hello $getNameFromInput"
+
+        txtInputName.setText("")
+    }
+
 }
