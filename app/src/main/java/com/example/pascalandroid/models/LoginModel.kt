4a85a1ea2093b624f7001e31ff7270ebff11b0cb
Jution Candra Kirana <jutionck@gmail.com>
2021-11-17 18:39:57 +0700

    [FRAGMENT] Initial Project

--- app/src/main/java/com/example/pascalandroid/models/LoginModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pascalandroid.models
-
-data class LoginModel(
-    val username: String,
-    val password: String
-)
--- app/src/main/java/com/example/pascalandroid/models/RegisterModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pascalandroid.models
-
-data class RegisterModel(
-    val fullName: String,
-    val username: String,
-    val email: String,
-    val password: String
-)
