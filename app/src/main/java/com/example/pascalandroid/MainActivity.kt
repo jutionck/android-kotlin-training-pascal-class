fb469c40c37d42eb23b3cffbd2372ea0ac9cf7b6
Jution Candra Kirana <jutionck@gmail.com>
2021-11-18 13:19:51 +0700

    [FRAGMENT] Hands on counter fragment manager

--- app/src/main/java/com/example/pascalandroid/MainActivity.kt
+++ app/src/main/java/com/example/pascalandroid/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.pascalandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.pascalandroid.databinding.ActivityMainBinding
+import com.example.pascalandroid.screen.CounterFragment
+import com.example.pascalandroid.screen.CounterShowFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fragmentCounter: CounterFragment
+    private lateinit var fragmentCounterShow: CounterShowFragment
+    private var counter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        fragmentCounter = CounterFragment()
+        fragmentCounterShow = CounterShowFragment()
+        with(binding) {
+            supportFragmentManager.beginTransaction().add(counterFragment.id, fragmentCounter).commit()
+            supportFragmentManager.beginTransaction().add(counterShowFragment.id, fragmentCounterShow).commit()
+        }
+    }
+
+    fun notifyIncrease() {
+        counter++
+        Log.i("test", "NotifyIncrease $counter")
+        fragmentCounterShow.notifyShowCounter(counter)
+    }
+
+    fun notifyDecrease() {
+        counter--
+        Log.i("test", "NotifyDecrease $counter")
+        fragmentCounterShow.notifyShowCounter(counter)
     }
 }
---
+++ app/src/main/java/com/example/pascalandroid/screen/CounterShowFragment.kt
@@ -0,0 +1,43 @@
+package com.example.pascalandroid.screen
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.pascalandroid.databinding.FragmentCounterShowBinding
+
+class CounterShowFragment : Fragment() {
+
+    private var _binding: FragmentCounterShowBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding =  FragmentCounterShowBinding.inflate(inflater, container, false)
+        Log.i("test", "onCreateView() called $_binding")
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun notifyShowCounter(counter: Int) {
+       binding.tvCounter.text = counter.toString()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
